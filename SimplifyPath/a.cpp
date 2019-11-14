12e9dbf2ade8a2bc32521a71c8470d145e63a1e2
shirish111 <shirishtogarla533@gmail.com>
2019-11-14 21:44:10 +0530

    Added SimplifyPath

---
+++ SimplifyPath/a.cpp
@@ -0,0 +1,114 @@
+// Simplify Path
+
+// Given an absolute path for a file (Unix-style), simplify it. Or in other
+// words, convert it to the canonical path.
+
+// In a UNIX-style file system, a period . refers to the current directory.
+// Furthermore, a double period .. moves the directory up a level. For more
+// information, see: Absolute path vs relative path in Linux/Unix
+
+// Note that the returned canonical path must always begin with a slash /, and
+// there must be only a single slash / between two directory names. The last
+// directory name (if it exists) must not end with a trailing /. Also, the
+// canonical path must be the shortest string representing the absolute path.
+
+// Example 1:
+
+// Input: "/home/"
+// Output: "/home"
+// Explanation: Note that there is no trailing slash after the last directory
+// name. Example 2:
+
+// Input: "/../"
+// Output: "/"
+// Explanation: Going one level up from the root directory is a no-op, as the
+// root level is the highest level you can go. Example 3:
+
+// Input: "/home//foo/"
+// Output: "/home/foo"
+// Explanation: In the canonical path, multiple consecutive slashes are replaced
+// by a single one. Example 4:
+
+// Input: "/a/./b/../../c/"
+// Output: "/c"
+// Example 5:
+
+// Input: "/a/../../b/../c//.//"
+// Output: "/c"
+// Example 6:
+
+// Input: "/a//b////c/d//././/.."
+// Output: "/a/b/c"
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define X first
+#define Y second
+#define PB push_back
+#define F0(I, N) for (ll I = 0; I < N; I++)
+#define F1(I, N) for (ll I = 1; I <= N; I++)
+#define F(I, X, N) for (ll I = X; I < N; I++)
+#define R0(I, N) for (ll I = N - 1; I >= 0; I--)
+#define R1(I, N) for (ll I = N; I > 0; I--)
+#define R(I, X, N) for (ll I = N - 1; I >= X; I--)
+#define A(X) X.begin(), X.end()
+
+class Solution {
+ public:
+  string simplifyPath(string &path) {
+    int n = path.length();
+    vector<string> v;
+    stringstream ss(path);
+    string s;
+    while (getline(ss, s, '/')) {
+      if (s == "" || s == ".") {
+        continue;
+      } else if (s == "..") {
+        if (!v.empty()) {
+          v.pop_back();
+        }
+      } else {
+        v.push_back(s);
+      }
+    }
+    string ans;
+    for (auto &i : v) {
+      ans.append("/" + i);
+    }
+    return ans == "" ? "/" : ans;
+  }
+};
+void solve() {
+  vector<string> tc = {
+      "/home/",
+      "/../",
+      "/home//foo/",
+      "/a/./b/../../c/",
+      "/a/../../b/../c//.//",
+      "/a//b////c/d//././/..",
+      "///////RgQ/DCkMtxRQArpnS///./../.././////lU/HkOPFethUYRJYwGjzHMe/"
+      "rJteBUS///IsKEnIQJ/../../hGXBMGFqtOjs/rmSJGOpIwm/CLuUcSXPiwjsK///"
+      "uAmXcWR/./tTrlKBgzBlNEwF/OOpDjmMoTvaedK///./YYjtE///cH/KOmxKcGmWg/"
+      "qGCOBeWiABLFqcIOmz/////./maSVbpF/./CWFbBs/qMzsrWxv/.././vvUmYui///"
+      "KCVaRfmLpZOzberV/VHOA/jphGcKrSF/kJkDRYKtpUosUWVbD///"
+      "gclGacfgDoPGdiXxPWyc/MZIcVQZzab/DIGNdzzS/qBPDkxjWjEQlv/Njfre/./hxoM/"
+      "mKKOwBVEWZwjagOuWi/UuLHfsWzeWi/../QuopYsUtqgnqAiXwE/qgokdsKKhosJhdBiSjA/"
+      "..///ANrXgVcSNejG/FIfSyTz/.///UZlMfMXBUmuCDhiVQf///kqdiy/aRxweiOpLcczy/"
+      "RPed/e/zbO/././M/NxCFeiNOJI/../qfBhtXbBFuRBPQrYpTx///"
+      "tZhVBcmIMlnygAKHKdcT///rQllOCPKsfmXOdMT/./xqXODEft/dOqZbqJ/uCii///../../"
+      "fDWbSnpMzPd///./PS/.././//HIQJuNdVfdlpqBKA/wo/.././OhBzBcujoxcpY/"
+      "EHrtJtqIsnhIUkqh///./../WKPXCkCl/FEncfxOdTdcBPMUAS/OYcSyadfthTntUAsjUm/"
+      "././iTURgskcGXkmDKWLcis/KvNnwUvQcCBJ/UCNcxJzeOdaX/././..///tX///"
+      "DfxuGi/"};
+  Solution s;
+  for (auto &i : tc) {
+    cout << s.simplifyPath(i) << "\n";
+  }
+}
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  solve();
+  return 0;
+}
