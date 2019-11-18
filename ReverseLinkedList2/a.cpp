fc08b2216301dcf518403580b0ea91de09b9d4e7
shirish111 <shirishtogarla533@gmail.com>
2019-11-18 22:04:44 +0530

    Added ReverseLinkedList2

---
+++ ReverseLinkedList2/a.cpp
@@ -0,0 +1,95 @@
+// Reverse Linked List II
+
+// Reverse a linked list from position m to n. Do it in one-pass.
+
+// Note: 1 ≤ m ≤ n ≤ length of list.
+
+// Example:
+
+// Input: 1->2->3->4->5->NULL, m = 2, n = 4
+// Output: 1->4->3->2->5->NULL
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define X first
+#define Y second
+#define PB push_back
+#define F0(I, N) for (ll I = 0; I < N; I++)
+#define F1(I, N) for (ll I = 1; I <= N; I++)
+#define F(I, X, N) for (ll I = X; I < N; I++)
+#define R0(I, N) for (ll I = N - 1; I >= 0; I--)
+#define R1(I, N) for (ll I = N; I > 0; I--)
+#define R(I, X, N) for (ll I = N - 1; I >= X; I--)
+#define A(X) X.begin(), X.end()
+
+// Definition for singly-linked list.
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+ public:
+  ListNode* reverseBetween(ListNode* head, int m, int n) {
+    if (!head) {
+      return head;
+    }
+    ListNode *p, *q, *r;
+    ListNode* prehead = new ListNode(-1);
+    prehead->next = head;
+    r = q = prehead;
+    p = head;
+    for (int i = 1; i < m; i++) {
+      r = q = p;
+      p = p->next;
+    }
+    for (int i = m; i <= n; i++) {
+      ListNode* tmp = p->next;
+      p->next = q;
+      q = p;
+      p = tmp;
+    }
+    r->next->next = p;
+    r->next = q;
+    return prehead->next;
+  }
+};
+ListNode* buildList(vector<int>& v) {
+  ListNode *head = nullptr, *p;
+  for (const auto& i : v) {
+    if (head == nullptr) {
+      head = new ListNode(i);
+      p = head;
+    } else {
+      p->next = new ListNode(i);
+      p = p->next;
+    }
+  }
+  return head;
+}
+void printList(ListNode* l) {
+  ListNode* p = l;
+  while (p) {
+    cout << p->val << " ";
+    p = p->next;
+  }
+  cout << "\n";
+}
+void solve() {
+  vector<pair<vector<int>, pair<int, int>>> tc = {{{1, 2, 3, 4, 5}, {2, 4}}};
+  Solution s;
+  for (auto& i : tc) {
+    ListNode* l = buildList(i.first);
+    printList(l);
+    l = s.reverseBetween(l, i.second.first, i.second.second);
+    printList(l);
+  }
+}
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  solve();
+  return 0;
+}
