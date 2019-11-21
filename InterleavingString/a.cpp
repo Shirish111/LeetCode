831f9c8f485d2cdbbec735853577a031e6299fe8
shirish111 <shirishtogarla533@gmail.com>
2019-11-21 21:43:50 +0530

    Added InterleavingString

---
+++ InterleavingString/a.cpp
@@ -0,0 +1,76 @@
+// Interleaving String
+
+// Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+
+// Example 1:
+
+// Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+// Output: true
+// Example 2:
+
+// Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+// Output: false
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define X first
+#define Y second
+#define PB push_back
+#define F0(I, N) for (ll I = 0; I < N; I++)
+#define F1(I, N) for (ll I = 1; I <= N; I++)
+#define F(I, X, N) for (ll I = X; I < N; I++)
+#define R0(I, N) for (ll I = N - 1; I >= 0; I--)
+#define R1(I, N) for (ll I = N; I > 0; I--)
+#define R(I, X, N) for (ll I = N - 1; I >= X; I--)
+#define A(X) X.begin(), X.end()
+
+class Solution {
+ public:
+  int n1, n2, n3;
+  vector<vector<int>> dp;
+  bool isInterleave(string& s1, string& s2, string& s3) {
+    n1 = s1.length();
+    n2 = s2.length();
+    n3 = s3.length();
+    if (n1 + n2 != n3) {
+      return false;
+    }
+    dp.assign(n1 + 1, vector<int>(n2 + 1, -1));
+    return isInterleave(s1, s2, s3, 0, 0, 0);
+  }
+  bool isInterleave(string& s1, string& s2, string& s3, int i, int j, int k) {
+    if (dp[i][j] != -1) {
+      return dp[i][j];
+    }
+    if (k == n3) {
+      return i == n1 && j == n2;
+    }
+    if (i < n1 && s1[i] == s3[k]) {
+      if (isInterleave(s1, s2, s3, i + 1, j, k + 1)) {
+        return true;
+      }
+    }
+    if (j < n2 && s2[j] == s3[k]) {
+      if (isInterleave(s1, s2, s3, i, j + 1, k + 1)) {
+        return true;
+      }
+    }
+    dp[i][j] = 0;
+    return false;
+  }
+};
+void solve() {
+  vector<vector<string>> tc = {{"aabcc", "dbbca", "aadbbcbcac"},
+                               {"aabcc", "dbbca", "aadbbbaccc"}};
+  Solution s;
+  for (auto& i : tc) {
+    cout << boolalpha << s.isInterleave(i[0], i[1], i[2]) << "\n";
+  }
+}
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  solve();
+  return 0;
+}
