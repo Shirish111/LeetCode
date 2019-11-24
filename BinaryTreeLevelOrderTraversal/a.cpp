a4d30382a08a3d79c8905939447b5530f47c567d
shirish111 <shirishtogarla533@gmail.com>
2019-11-24 08:16:30 +0530

    Added BinaryTreeLevelOrderTraversal

---
+++ BinaryTreeLevelOrderTraversal/a.cpp
@@ -0,0 +1,75 @@
+// Binary Tree Level Order Traversal
+
+// Given a binary tree, return the level order traversal of its nodes' values.
+// (ie, from left to right, level by level).
+
+// For example:
+// Given binary tree [3,9,20,null,null,15,7],
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// return its level order traversal as:
+// [
+//   [3],
+//   [9,20],
+//   [15,7]
+// ]
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define X first
+#define Y second
+#define PB push_back
+#define F0(I, N) for (ll I = 0; I < N; I++)
+#define F1(I, N) for (ll I = 1; I <= N; I++)
+#define F(I, X, N) for (ll I = X; I < N; I++)
+#define R0(I, N) for (ll I = N - 1; I >= 0; I--)
+#define R1(I, N) for (ll I = N; I > 0; I--)
+#define R(I, X, N) for (ll I = N - 1; I >= X; I--)
+#define A(X) X.begin(), X.end()
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+ public:
+  vector<vector<int>> levelOrder(TreeNode *root) {
+    vector<vector<int>> ans;
+    if (root == nullptr) {
+      return ans;
+    }
+    queue<pair<TreeNode *, int>> q;
+    pair<TreeNode *, int> p;
+    q.push({root, 0});
+    while (!q.empty()) {
+      p = q.front();
+      q.pop();
+      if (ans.size() == p.second) {
+        ans.push_back(vector<int>());
+      }
+      ans[p.second].push_back(p.first->val);
+      if (p.first->left) {
+        q.push({p.first->left, p.second + 1});
+      }
+      if (p.first->right) {
+        q.push({p.first->right, p.second + 1});
+      }
+    }
+    return ans;
+  }
+};
+void solve() {}
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  solve();
+  return 0;
+}
