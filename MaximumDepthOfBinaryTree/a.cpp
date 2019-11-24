f365edd2754b0f284eb32f5fccab3cddc879ebae
shirish111 <shirishtogarla533@gmail.com>
2019-11-24 11:04:47 +0530

    Added MaximumDepthOfBinaryTree

---
+++ MaximumDepthOfBinaryTree/a.cpp
@@ -0,0 +1,58 @@
+// Maximum Depth of Binary Tree
+
+// Given a binary tree, find its maximum depth.
+
+// The maximum depth is the number of nodes along the longest path from the root
+// node down to the farthest leaf node.
+
+// Note: A leaf is a node with no children.
+
+// Example:
+
+// Given binary tree [3,9,20,null,null,15,7],
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// return its depth = 3.
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define X first
+#define Y second
+#define PB push_back
+#define F0(I, N) for (ll I = 0; I < N; I++)
+#define F1(I, N) for (ll I = 1; I <= N; I++)
+#define F(I, X, N) for (ll I = X; I < N; I++)
+#define R0(I, N) for (ll I = N - 1; I >= 0; I--)
+#define R1(I, N) for (ll I = N; I > 0; I--)
+#define R(I, X, N) for (ll I = N - 1; I >= X; I--)
+#define A(X) X.begin(), X.end()
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+ public:
+  int maxDepth(TreeNode *root) {
+    if (root == nullptr) {
+      return 0;
+    }
+    return 1 + max(maxDepth(root->left), maxDepth(root->right));
+  }
+};
+void solve() {}
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  solve();
+  return 0;
+}
