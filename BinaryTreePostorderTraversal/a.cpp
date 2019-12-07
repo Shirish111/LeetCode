b55810cdbb56ae314bc1f490f61d02d6321e60e4
shirish111 <shirishtogarla533@gmail.com>
2019-12-07 15:41:12 +0530

    Added BinaryTreePostorderTraversal

---
+++ BinaryTreePostorderTraversal/a.cpp
@@ -0,0 +1,70 @@
+// Binary Tree Postorder Traversal
+
+// Given a binary tree, return the postorder traversal of its nodes' values.
+
+// Example:
+
+// Input: [1,null,2,3]
+//    1
+//     \
+//      2
+//     /
+//    3
+
+// Output: [3,2,1]
+// Follow up: Recursive solution is trivial, could you do it iteratively?
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define X first
+#define Y second
+#define PB push_back
+#define F0(I, N) for (ll I = 0; I < N; I++)
+#define F1(I, N) for (ll I = 1; I <= N; I++)
+#define F(I, X, N) for (ll I = X; I < N; I++)
+#define R0(I, N) for (ll I = N - 1; I >= 0; I--)
+#define R1(I, N) for (ll I = N; I > 0; I--)
+#define R(I, X, N) for (ll I = N - 1; I >= X; I--)
+#define A(X) X.begin(), X.end()
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+ public:
+  vector<int> postorderTraversal(TreeNode *root) {
+    vector<int> ans;
+    if (!root) {
+      return ans;
+    }
+    TreeNode *p = root;
+    stack<TreeNode *> s1;
+    s1.push(p);
+    while (!s1.empty()) {
+      p = s1.top();
+      s1.pop();
+      ans.push_back(p->val);
+      if (p->left) {
+        s1.push(p->left);
+      }
+      if (p->right) {
+        s1.push(p->right);
+      }
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+  }
+};
+void solve() {}
+int main() {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  solve();
+  return 0;
+}
