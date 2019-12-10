3c6f7a114a9a729c8fd98d39a50cd94a79fe576d
shirish111 <shirishtogarla533@gmail.com>
2019-12-10 22:28:53 +0530

    Added FactorialTrailingZeroes

---
+++ FactorialTrailingZeroes/a.cpp
@@ -0,0 +1,54 @@
+// Factorial Trailing Zeroes
+
+// Given an integer n, return the number of trailing zeroes in n!.
+
+// Example 1:
+
+// Input: 3
+// Output: 0
+// Explanation: 3! = 6, no trailing zero.
+// Example 2:
+
+// Input: 5
+// Output: 1
+// Explanation: 5! = 120, one trailing zero.
+// Note: Your solution should be in logarithmic time complexity.
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define X first
+#define Y second
+#define PB push_back
+#define F0(I, N) for (ll I = 0; I < N; I++)
+#define F1(I, N) for (ll I = 1; I <= N; I++)
+#define F(I, X, N) for (ll I = X; I < N; I++)
+#define R0(I, N) for (ll I = N - 1; I >= 0; I--)
+#define R1(I, N) for (ll I = N; I > 0; I--)
+#define R(I, X, N) for (ll I = N - 1; I >= X; I--)
+#define A(X) X.begin(), X.end()
+
+class Solution {
+ public:
+  int trailingZeroes(int n) {
+    int cnt = 0;
+    while (n) {
+      cnt += n / 5;
+      n /= 5;
+    }
+    return cnt;
+  }
+};
+void solve() {
+  vector<int> tc = {3, 5, 10, 15, 22};
+  Solution s;
+  for (auto &i : tc) {
+    cout << s.trailingZeroes(i) << "\n";
+  }
+}
+int main() {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  solve();
+  return 0;
+}
